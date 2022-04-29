63474d52b9a2ac5d69962a03f2a3b473d25335fb
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-04-29 15:39:19 +0300

    test code

--- HTMLTemplateGenerator/Program.cs
+++ HTMLTemplateGenerator/Program.cs
@@ -1,15 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+// Author: Dashie
+
+
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HTMLTemplateGenerator
 {
-    internal class Program
+    public class HTML
     {
-        static void Main(string[] args)
+        public string Source { get; set; } = string.Empty;
+        public void SetSource(StringBuilder builder) => Source = builder.ToString();
+    }
+    
+    public partial class HTMLTemplates
+    {
+        public HTML GenerateREADME1(string h2Title, string[] h4Summary, string[] pDescription, string[] pLastWords)
         {
+            // Information based README
+
+            return new HTML();
+        }
+
+        public HTML GenerateREADME2(string h2Title, string[] h4Summary, string[] pTableDescription, string[] tableRows, string[] tableColumns, string[] pLastWords)
+        {
+            // Table based README
+
+            return new HTML();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HTMLTemplates t = new();
+
+            string html1 = t.GenerateREADME1(
+                "", 
+                new string[] { },
+                new string[] { },
+                new string[] { }
+            ).Source;
+
+            string html2 = t.GenerateREADME2(
+                "",
+                new string[] { },
+                new string[] { },
+                new string[] { },
+                new string[] { },
+                new string[] { }
+            ).Source;
+
+            File.WriteAllText("html1.html", html1);
+            File.WriteAllText("html2.html", html2);
         }
     }
 }
