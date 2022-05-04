19e3c66fd2cf2edb6c768ba2906257ea046dddce
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-05-04 16:23:31 +0300

    start of video helper
    This little thing will be a revamp of what I made before. It basically is a tool to help me get albums out of my door more easily.

---
+++ VideoHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Author: Dashie
+
+
+namespace VideoHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A class/program to help me with managing video related shits because I have so much shit to upload.
+            int value = args switch
+            {
+                null => 0//C# latest test
+            };
+        }
+    }
+}
