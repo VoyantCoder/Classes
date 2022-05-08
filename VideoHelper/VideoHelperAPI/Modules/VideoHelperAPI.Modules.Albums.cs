187011bd37899604079b0e7e05967eff1a9c1d14
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-05-08 19:14:15 +0300

    modules for listing albums

---
+++ VideoHelper/VideoHelperAPI/Modules/VideoHelperAPI.Modules.Albums.cs
@@ -0,0 +1,52 @@
+﻿
+// Author: Dashie
+
+
+using System.Linq;
+using System.Collections.Generic;
+using static System.IO.Directory;
+
+
+namespace VideoHelper
+{
+    public partial class VideoHelperAPI
+    {
+        public bool AlbumExists(string workspace, string name)
+        {
+            if (ValidateWorkspace(workspace))
+            {
+                return Exists($@"{workspace}\{name}");
+            }
+
+            return false;
+        }
+
+        public bool AlbumExists(string name) => AlbumExists(workspace, name);
+
+        public string[] ListAlbums(string workspace)
+        {
+            if (ValidateWorkspace(workspace))
+            {
+                IEnumerable<string> Albums()
+                {
+                    foreach (string directory in GetDirectories(workspace))
+                    {
+                        if (Exists($@"{directory}\rendered"))
+                        {
+                            yield return directory.StartsWith("./") ? directory.Replace("./", "") : directory.Split('\\').Last();
+                        }
+                    }
+                }
+
+                return Albums().ToArray();
+            }
+
+            return new[] 
+            { 
+                VideoHelperResult.WorkspaceDoesNotExist.ToString()
+            };
+        }
+
+        public string[] ListAlbums() => ListAlbums(workspace);
+    }
+}
