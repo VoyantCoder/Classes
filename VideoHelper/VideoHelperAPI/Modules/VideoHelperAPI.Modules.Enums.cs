d4a464de3bd3ddfff84bcf4dfbbd353c68911551
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-05-08 19:14:50 +0300

    predefined return values added

---
+++ VideoHelper/VideoHelperAPI/Modules/VideoHelperAPI.Modules.Enums.cs
@@ -0,0 +1,19 @@
+﻿
+// Author: Dashie
+
+
+namespace VideoHelper
+{
+    public enum VideoHelperResult
+    {
+        DirectoryAlreadyExists, 
+        
+        WorkspaceIsInvalid, WorkspaceDoesNotExist,
+        
+        UnhandledCreationError, UnhandledRenameError,
+        
+        Success,
+        
+        AlbumDoesNotExist, AlbumAlreadyExists, AlbumRenameFailed
+    }
+}
