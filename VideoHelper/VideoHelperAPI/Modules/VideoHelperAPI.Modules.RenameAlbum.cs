2e5d916c88e732d08887b3260be91263ff50d9b2
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-05-08 19:15:00 +0300

    added module for renaming albums

---
+++ VideoHelper/VideoHelperAPI/Modules/VideoHelperAPI.Modules.RenameAlbum.cs
@@ -0,0 +1,47 @@
+﻿
+// Author: Dashie
+
+
+using static System.IO.Directory;
+
+
+namespace VideoHelper
+{
+    public partial class VideoHelperAPI
+    {
+        public VideoHelperResult RenameAlbum(string workspace, string album, string newName)
+        {// LEFT OFF AT EXPLANATION WHAT YOU ADDED (RenameAlbum and Additionalities)
+            try
+            {
+                if (!ValidateWorkspace(workspace))
+                {
+                    return VideoHelperResult.WorkspaceIsInvalid;
+                }
+
+                else if (AlbumExists(workspace, album))
+                {
+                    if (AlbumExists(workspace, newName))
+                    {
+                        return VideoHelperResult.AlbumAlreadyExists;
+                    }
+
+                    Move($@"{workspace}\{album}", $@"{workspace}\{newName}");
+
+                    if (AlbumExists(workspace, newName))
+                    {
+                        return VideoHelperResult.Success;
+                    }
+                }
+            }
+
+            catch
+            {
+                return VideoHelperResult.UnhandledRenameError;
+            }
+
+            return VideoHelperResult.AlbumRenameFailed;
+        }
+
+        public VideoHelperResult RenameAlbum(string album, string newName) => RenameAlbum(workspace, album, newName);
+    }
+}
