c6a81d2baaad3b47048f1ee5986f1eb53e62218a
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-05-08 19:15:12 +0300

    added modules for the validation of the current workspace

---
+++ VideoHelper/VideoHelperAPI/Modules/VideoHelperAPI.Modules.Workspaces.cs
@@ -0,0 +1,37 @@
+﻿
+// Author: Dashie
+
+
+using static System.IO.Directory;
+
+
+namespace VideoHelper
+{
+    public partial class VideoHelperAPI
+    {
+        private string workspace = string.Empty; // V:\YouTube\Recording\Albums
+
+        public string Workspace
+        {
+            get
+            {
+                return workspace;
+            }
+
+            set
+            {
+                if (Exists(value))
+                {
+                    workspace = value.Replace("/", @"\").EndsWith(@"\") ? value.Substring(value.Length - 1, 1) : value;
+                }
+            }
+        }
+
+        private bool ValidateWorkspace(string workspace)
+        {
+            return workspace != null && workspace != string.Empty && Exists(workspace);
+        }
+
+        private bool ValidateWorkspace() => ValidateWorkspace(workspace);
+    }
+}
