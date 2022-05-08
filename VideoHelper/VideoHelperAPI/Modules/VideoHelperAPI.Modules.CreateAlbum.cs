45ac04f5ed58ec365b2410224b706c14292c9815
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-05-08 19:14:28 +0300

    module for creating an album folder structure

---
+++ VideoHelper/VideoHelperAPI/Modules/VideoHelperAPI.Modules.CreateAlbum.cs
@@ -0,0 +1,48 @@
+﻿
+// Author: Dashie
+
+
+using static System.IO.Directory;
+
+
+namespace VideoHelper
+{
+    public partial class VideoHelperAPI
+    {
+        public VideoHelperResult CreateAlbum(string name, string workspace)
+        {
+            try
+            {
+                if (!ValidateWorkspace(workspace) || AlbumExists(name))
+                {
+                    return VideoHelperResult.WorkspaceIsInvalid;
+                }
+
+                var paths = new[]
+                {
+                    $@"{workspace}\{name}",
+                    $@"{workspace}\{name}\rendered"
+                };
+
+                foreach (string path in paths)
+                {
+                    if (Exists(path))
+                    {
+                        return VideoHelperResult.DirectoryAlreadyExists;
+                    }
+
+                    CreateDirectory(path);
+                }
+            }
+
+            catch
+            {
+                return VideoHelperResult.UnhandledCreationError;
+            }
+
+            return VideoHelperResult.Success;
+        }
+
+        public void CreateAlbum(string name) => CreateAlbum(name, workspace);
+    }
+}
