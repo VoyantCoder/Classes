7a7e952794dd368df0b562d28c6a102471b40b5d
VoyantCoderoi <60381639+VoyantCoder@users.noreply.github.com>
2022-07-13 22:29:10 +0300

    open urls
    simple man

---
+++ URLOpener/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Author: Dashie
+
+
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+// Summary: This class deals with the opening of URLs.
+
+namespace URLOpener
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0 && File.Exists(args[0]))
+            {
+                if (args[0].ToLower().EndsWith(".txt"))
+                {
+                    var response = MessageBox.Show("You are about to open URLs. Are you sure?", "URL Opener", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (response != DialogResult.Yes)
+                    {
+                        return;
+                    }
+
+                    foreach (string line in File.ReadLines(args[0]))
+                    {
+                        Process.Start(line);
+                    }
+                }
+            }
+        }
+    }
+}
